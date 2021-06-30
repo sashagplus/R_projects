e6a6acca6bedb55c52e7c26b20dac44c730bbbee
sashagplus <sasha@genetikaplus.com>
2021-06-30 14:29:19 +0300

    availability rnaseq and imaging and congruent with how many added in each Working version! (tfu tfu tfu)

--- quick_and_dirty/20210518_data_availability.R
+++ quick_and_dirty/20210518_data_availability.R
@@ -6,10 +6,11 @@ library("rpart")
 library(rpart.plot)
 library(readxl)    
 library(caret)
+library(flextable)
 
 rm(list=ls())
 
-extract_date="20210705" #Sys.Date()
+extract_date=Sys.Date()
 #return the closest PREVIOUS weekday to curr_date
 #if weekdays(curr_date)=="Monday" then return the date of previous weekday
 getDateOfLatestWeekday=function(curr_date=Sys.Date(),
@@ -24,6 +25,8 @@ getDateOfLatestWeekday=function(curr_date=Sys.Date(),
   return(prev.days[weekdays(prev.days)==weekday])
 }
 
+
+current_treatments=c("BUP", "MIRT", "CIT", "NTP", "VN")
 
 #0. Read stard lines to g+ lines mapping
 #gp = genetikaplus
@@ -108,7 +111,7 @@ imaging_data.LineTreat=imaging_data %>% select(Line, Treat, isNew) %>% distinct(
 #imaging_data.LineTreat %<>% filter(Treat!="VEH") %>% left_join(patient_to_sample %>% select(id.stard, id.gp.numeric), by=c("Line"="id.gp.numeric")) 
 
 
-pts=merge(patient_to_sample %>% select(id.stard, id.gp.numeric), data.frame(Treat.abbrv=c("BUP", "MIRT", "CIT", "NTP", "VN")))
+pts=merge(patient_to_sample %>% select(id.stard, id.gp.numeric), data.frame(Treat.abbrv=current_treatments))
 pts %<>% filter(!(id.gp.numeric==92 & Treat.abbrv=="MIRT")) #Why?
 pts %<>%  left_join(rnaseq_data.LineTreat, by=c(c("id.gp.numeric"="CellLIne.numeric", "Treat.abbrv"="Treatment.abbrv"))) 
 
@@ -123,160 +126,108 @@ pts %<>% left_join(responsSelected, by=c("id.stard"="Patient.ID", "Treat.abbrv")
 
 pts %<>% filter(!is.na(rnaseq) | !is.na(imaging))
 
-totals_by_tretment_and_analysis=pts %>% group_by(rnaseq, imaging, Treat.abbrv) %>% 
-  tally() %>% 
-  spread(key=Treat.abbrv, value=n) %>% 
-  ungroup %>% bind_rows(summarise_all(., ~if(is.numeric(.)) sum(.) else "Total"))
+#!!!TEMP
+#pts[16:21, "rnaseq_oon"]="rnaseq_new"; pts[18:22, "imaging_oon"]="imaging_new"
+
+data_avaialble_response=pts %>% group_by(Treat.abbrv, response_imp50p, rnaseq_oon, imaging_oon) %>% tally() %>%
+                                dplyr::mutate_at(.vars=dplyr::vars("rnaseq_oon", "imaging_oon"), ~ifelse(is.na(.x), "", .x)) %>% dplyr::ungroup() %>%
+                                dplyr::mutate(analysis_type_str=stringr::str_c(rnaseq_oon, "XXX", imaging_oon), .keep="unused") %>%
+                                tidyr::spread(key="analysis_type_str", value=n)
+
+data_avaialble_response %<>% dplyr::mutate(both=rowSums(dplyr::select(dplyr::select(., dplyr::starts_with("rnaseq_")), dplyr::contains("imaging")), na.rm = T)) %>%
+                            dplyr::mutate(imaging=rowSums(dplyr::select(., dplyr::starts_with("XXXimaging")), na.rm = T),
+                                          rnaseq=rowSums(dplyr::select(., dplyr::ends_with("XXX")), na.rm = T)) 
+
+#calculate totals per treatment and response:
+data_avaialble_response %<>% dplyr::mutate(total=rowSums(dplyr::select(., both, imaging, rnaseq), na.rm = T))
+
+treatment.sum=data_avaialble_response %>% dplyr::group_by(Treat.abbrv) %>% 
+                                          dplyr::summarise_at(.vars=dplyr::vars(both, imaging, rnaseq, total), ~sum(.x, na.rm=T)) %>% 
+                                          dplyr::ungroup()
+
+#data_avaialble_response %<>% dplyr::select(-rnaseq_oldXXXimaging_old,  -rnaseq_oldXXX, -XXXimaging_old)
+
+data_avaialble_response %<>% dplyr::mutate(both_="",
+                                           rnaseq_="",
+                                           imaging_="")
 
 
-#pts %<>% gather(key="AnalysisType", value="cnt", -id.stard, -id.gp.numeric, -Treat.abbrv, -response) %>% drop_na(cnt) 
+# define brackets
+# 1. both, going over all options ([rna new,img new], [rna old, img new], [rna new, img old] )
+if ("rnaseq_newXXXimaging_new" %in% colnames(data_avaialble_response)) data_avaialble_response %<>% 
+                                                    dplyr::mutate(both_=ifelse(!is.na(rnaseq_newXXXimaging_new), 
+                                                                               stringr::str_c(both_, "+", rnaseq_newXXXimaging_new, " both"), 
+                                                                               ""))
 
-#data_avaialble=pts %>% group_by(AnalysisType, Treat.abbrv, response) %>% summarise(sumcnt=sum(cnt), .groups = "keep") %>% spread(key=Treat.abbrv, value=sumcnt)
+if ("rnaseq_oldXXXimaging_new" %in% colnames(data_avaialble_response)) data_avaialble_response %<>% 
+                                                      dplyr::mutate(both_=ifelse(!is.na(rnaseq_oldXXXimaging_new), 
+                                                                                 stringr::str_c(both_, 
+                                                                                                ifelse(both_!="", ",", "")
+                                                                                                , "+", 
+                                                                                                rnaseq_oldXXXimaging_new, 
+                                                                                                " img"), 
+                                                                                 both_))
 
-data_avaialble_response=pts %>% group_by(rnaseq, imaging, Treat.abbrv, response_imp50p) %>% 
-                                mutate(analysis_type=ifelse((!is.na(rnaseq) & !is.na(imaging)), "both", ifelse(!is.na(rnaseq), rnaseq, imaging))) %>% 
-                                mutate(analysis_type_isNew=ifelse(analysis_type=="both" & (isNew.imaging==TRUE | isNew.rnaseq==TRUE), "new", "old")) %>% 
-                                mutate(analysis_type_isNew=ifelse(analysis_type=="imaging" & isNew.imaging==TRUE, "new", analysis_type_isNew)) %>% 
-                                mutate(analysis_type_isNew=ifelse(analysis_type=="rnaseq" & isNew.rnaseq==TRUE , "new", analysis_type_isNew)) %>% 
-                                group_by(Treat.abbrv, response_imp50p, analysis_type, analysis_type_isNew ) %>% tally() %>%
-                                spread(key=analysis_type_isNew, value=n)
-if("new" %in% colnames(data_avaialble_response))
-{
-  data_avaialble_response %<>% mutate(total=ifelse(is.na(new), 0, new)+ifelse(is.na(old), 0, old)) %>%
-                                mutate(n_string=stringr::str_c(total, ifelse(is.na(new), "", stringr::str_c(" (+", new, ")") ))) %>% 
-                                select(-old, -new, -total) %>% spread(key=analysis_type, value=n_string)
-} else
-{
-  data_avaialble_response %<>% spread(key=analysis_type, value=old)
-}
+if ("rnaseq_newXXXimaging_old" %in% colnames(data_avaialble_response)) data_avaialble_response %<>% 
+                                                      dplyr::mutate(both_=ifelse(!is.na(rnaseq_newXXXimaging_old), 
+                                                                                 stringr::str_c(both_, 
+                                                                                                ifelse(both_!="", ",", "")
+                                                                                                , "+", 
+                                                                                                rnaseq_newXXXimaging_old, 
+                                                                                                " rna"), 
+                                                                                 both_))
 
+# 2. rna
+if ("rnaseq_newXXX" %in% colnames(data_avaialble_response)) data_avaialble_response %<>% 
+                                                dplyr::mutate(rnaseq_=ifelse(!is.na(rnaseq_newXXX), 
+                                                                           stringr::str_c(rnaseq_, 
+                                                                                          ifelse(rnaseq_!="", ",", "")
+                                                                                          , "+", 
+                                                                                          rnaseq_newXXX, 
+                                                                                          " rna"), 
+                                                                           rnaseq_))
 
-data_avaialble_response=pts %>% group_by(rnaseq, imaging, Treat.abbrv, response_imp50p) %>% 
-              tally() %>% 
-              mutate(analysis_type=ifelse((!is.na(rnaseq) & !is.na(imaging)), "both", ifelse(!is.na(rnaseq), rnaseq, imaging))) %>% 
-              ungroup %>% 
-              select(-rnaseq, -imaging) %>% 
-              relocate(analysis_type) %>% 
-              mutate(treat_response=stringr::str_c(Treat.abbrv, "_", response_imp50p), .keep = c( "unused") ) %>% 
-              spread(key=treat_response, value=n)
-
-
-data_avaialble_remission=pts %>% group_by(rnaseq, imaging, Treat.abbrv, remission_p50imp_below6current) %>% 
-  tally() %>% 
-  mutate(analysis_type=ifelse((!is.na(rnaseq) & !is.na(imaging)), "both", ifelse(!is.na(rnaseq), rnaseq, imaging))) %>% 
-  ungroup %>% 
-  select(-rnaseq, -imaging) %>% 
-  relocate(analysis_type) %>% 
-  mutate(treat_response=stringr::str_c(Treat.abbrv, "_", remission_p50imp_below6current), .keep = c( "unused") ) %>% 
-  spread(key=treat_response, value=n)
-
-library(flextable)
-
-#explore R/NR ratios
-
-# flextable::flextable(data_avaialble) %>% 
-#   merge_v(~analysis_type) %>%
-#   set_header_labels(analysis_type="Analysis Type", response="R / NR") %>%
-#   align( i = NULL, j = NULL, align = "center", part="all") 
-
-# flextable::flextable(data_avaialble_response) %>% 
-#   merge_v(~analysis_type) %>%
-#   set_header_labels(analysis_type="Analysis Type", BUP_NONRESP="NR", BUP_RESP="R", CIT_NONRESP="NR", CIT_RESP="R", MIRT_NONRESP="NR",MIRT_RESP="R", NTP_NONRESP="NR", NTP_RESP="R") %>%
-#   add_header_row(values=c("", "BUP", "CIT", "MIRT", "NTP"), colwidths = c(1, 2,2, 2,2)) %>%
-#   add_header_row(values=c("RESPONSE"), colwidths = c(9)) %>%
-#   align( i = NULL, j = NULL, align = "center", part="all") %>% vline(j = c(1,3, 5,7,9), border = officer::fp_border(color="black", width = 1))
-# 
-# 
-# flextable::flextable(data_avaialble_remission) %>% 
-#   merge_v(~analysis_type) %>%
-#   set_header_labels(analysis_type="Analysis Type", BUP_NONREMS="NR", BUP_REMS="R", CIT_NONREMS="NR", CIT_REMS="R", MIRT_NONREMS="NR",MIRT_REMS="R", NTP_NONREMS="NR", NTP_REMS="R") %>%
-#   add_header_row(values=c("", "BUP", "CIT", "MIRT", "NTP"), colwidths = c(1, 2,2, 2,2)) %>%
-#   add_header_row(values=c("REMISSION"), colwidths = c(9)) %>%
-#   align( i = NULL, j = NULL, align = "center", part="all") %>% vline(j = c(1,3, 5,7,9), border = officer::fp_border(color="black", width = 1))
+# 3. imaging
+if ("XXXimaging_new" %in% colnames(data_avaialble_response)) data_avaialble_response %<>% 
+                                                dplyr::mutate(imaging_=ifelse(!is.na(XXXimaging_new), 
+                                                                             stringr::str_c(imaging_, 
+                                                                                            ifelse(imaging_!="", ",", "")
+                                                                                            , "+", 
+                                                                                            XXXimaging_new, 
+                                                                                            " img"), 
+                                                                             imaging_))
 
 
+data_avaialble_response %<>% dplyr::select(-dplyr::contains("XXX"))
 
-# remission_data=data_avaialble_remission %>% tibble::column_to_rownames("analysis_type") %>% 
-#                                             t %>% 
-#                                             as.data.frame() %>% 
-#                                             tibble::rownames_to_column("temp") %>%  
-#                                             tidyr::separate(col="temp", into=c("Treatment", "Remission"), sep="_") %>% 
-#                                             select(Treatment, Remission, remission_congruent=both, remission_rnaseq=rnaseq,  remission_imaging=imaging ) %>%
-#   rowwise() %>%
-#                                             mutate(remission_Total=sum(remission_congruent, remission_rnaseq, remission_imaging, na.rm = T))
+data_avaialble_response %<>% dplyr::mutate(both_str=as.character(ifelse(both_!="", stringr::str_c(both, "(", both_, ")"), both)),
+                                           imaging_str=as.character(ifelse(imaging_!="", stringr::str_c(imaging, "(", imaging_, ")"), imaging)),
+                                           rnaseq_str=as.character(ifelse(rnaseq_!="", stringr::str_c(rnaseq, "(", rnaseq_, ")"), rnaseq)))
 
-response_data=data_avaialble_response %>% tibble::column_to_rownames("analysis_type") %>% 
-                                          t %>% 
-                                          as.data.frame() %>% 
-                                          tibble::rownames_to_column("temp") %>%  
-                                          tidyr::separate(col="temp", into=c("Treatment", "Response"), sep="_") %>% 
-                                          select(Treatment, Response, response_congruent=both, response_rnaseq=rnaseq,  response_imaging=imaging ) %>%
-  rowwise() %>%
-mutate(response_Total=sum(response_congruent, response_rnaseq, response_imaging, na.rm = T))
+data_avaialble_response %<>% dplyr::select(-both, -imaging, -rnaseq, -both_, -rnaseq_, -imaging_)
 
-response_data %<>% group_by(Treatment) %>% summarise_at(.vars=vars(response_congruent,response_rnaseq,response_imaging,response_Total),
-                                                       ~ sum(.x, na.rm=T)) %>% 
-  mutate(Response="Total", .after=Treatment) %>% rowwise %>% mutate(response_rnaseq=response_rnaseq+response_congruent,
-                                                                    response_imaging=response_imaging+response_congruent) %>% 
-  bind_rows(response_data) %>% arrange(match(Treatment, c("BUP", "MIRT", "NTP", "CIT")), 
-                                       match(Response, c("NONRESP", "RESP", "Total")))
+print(1)
 
-# remission_data %<>% group_by(Treatment) %>% summarise_at(.vars=vars(remission_congruent,remission_rnaseq,remission_imaging,remission_Total),
-#                                                         ~ sum(.x, na.rm=T)) %>% 
-#   mutate(Remission="Total", .after=Treatment) %>% rowwise %>% mutate(remission_rnaseq=remission_rnaseq+remission_congruent,
-#                                                                      remission_imaging=remission_imaging+remission_congruent) %>% 
-#   bind_rows(remission_data) %>% arrange(match(Treatment, c("BUP", "MIRT", "NTP", "CIT")), 
-#                                        match(Remission, c("NONRESP", "RESP", "Total")))
+alldata=treatment.sum %>% dplyr::mutate(both_str=as.character(both),imaging_str=as.character(imaging), rnaseq_str=as.character(rnaseq), .keep="unused") %>% 
+                          dplyr::mutate(response_imp50p="Total") %>% dplyr::bind_rows(data_avaialble_response) %>% 
+                          dplyr::rename(Treatment=Treat.abbrv, Response=response_imp50p) %>%
+                          dplyr::relocate(Response, .after = Treatment) 
 
+print(2)
+alldata %<>% dplyr::arrange(Treatment, 
+                     match(Response, c("NONRESP", "RESP", "Total"))) %>%
+              dplyr::relocate(total, .after = dplyr::last_col())
 
-alldata=response_data %>% mutate(temp=ifelse(Response=="RESP", "REMS", ifelse(Response=="NONRESP" ,"NONREMS", "Total"))) %>% 
-  #right_join(remission_data, by=c("Treatment", "temp"="Remission")) %>% 
-  select(-temp)
-
-alldata %<>% arrange(match(Treatment, c("BUP", "MIRT", "NTP", "CIT")), 
-                     match(Response, c("NONRESP", "RESP", "Total")))
-
- # alldata %<>% rowwise() %>% mutate(Total=sum(response_congruent, response_rnaseq, response_imaging, na.rm = T)) %>%
- #             relocate("Total", .before=response_congruent)
-
-# ft_remission_and_response=
-#   flextable::flextable(alldata) %>% 
-# #  merge_v(~Treatment) %>%
-#   set_header_labels(Treatment="", 
-#                     Response="Response/ Remission Label", 
-#                     response_congruent="congruent", 
-#                     response_rnaseq="RNASeq",  
-#                     response_imaging="Imaging",
-#                     response_Total="Total",
-#                     remission_congruent="congruent", 
-#                     remission_rnaseq="RNASeq",  
-#                     remission_imaging="Imaging",
-#                     remission_Total="Total") %>%
-#   vline(j = c(5,9), border = officer::fp_border(color="grey", width = 1), part="all") %>%
-#   add_header_row(values=c("","","Response", "Remission"), colwidths = c(1, 1,4, 4)) %>%
-#   border_outer(part="all", border = officer::fp_border(color="red", width = 2) ) %>%
-#   add_header_row(values=c("Genetika+ Data Availability"), colwidths = c(10)) %>%
-#   align( i = NULL, j = NULL, align = "center", part="all") %>% 
-#   align( i = NULL, j = 2, align = "left", part="body") %>% 
-#   vline(j = c(2,6), border = officer::fp_border(color="black", width = 1), part="all") %>%
-#   hline(i=c(3,6, 9), border=officer::fp_border(color="lightgrey", width = 1, style="solid")) %>% 
-#   hline(i=c(2,5, 8), j=c(2:10), border=officer::fp_border(color="lightgrey", width = 1, style="dashed")) %>% 
-#   bold(  j = c(6, 10), bold = TRUE, part = "all") %>% 
-#   color(i = c(3,6,9), j = c(3,7), color="grey", part = "body", source = j) %>%
-#   merge_v(~Treatment) %>%
-#   fix_border_issues()
-#   
-# #save_as_pptx(ft, path="/Users/sashakugel/gplus_dropbox/Genetika+ Dropbox/Genetika+SharedDrive/01_Protocol_Development/01_10_Data_Science/Exploratory_Outputs/2021_05_30_data_availability.pptx")
 
 ft_response=
-  flextable::flextable(alldata %>% dplyr::select(-dplyr::starts_with("remission_"))) %>% 
+  flextable::flextable(alldata ) %>% 
   #  merge_v(~Treatment) %>%
-  set_header_labels(Treatment="", 
+  set_header_labels(Treatment="Treatment", 
                     Response="Response Label", 
-                    response_congruent="congruent", 
-                    response_rnaseq="RNASeq",  
-                    response_imaging="Imaging",
-                    response_Total="Total") %>%
+                    both_str="congruent", 
+                    rnaseq_str="RNASeq",  
+                    imaging_str="Imaging",
+                    total="Total") %>%
   vline(j = c(5), border = officer::fp_border(color="grey", width = 1), part="all") %>%
   add_header_row(values=c("","","Response"), colwidths = c(1, 1,4)) %>%
   border_outer(part="all", border = officer::fp_border(color="red", width = 2) ) %>%
@@ -288,6 +239,7 @@ ft_response=
   hline(i=c(2,5,8,13), j=c(2:6), border=officer::fp_border(color="lightgrey", width = 1, style="dashed")) %>% 
   bold(  j = c(6), bold = TRUE, part = "all") %>% 
   color(i = c(3,6,9,12), j = c(3), color="grey", part = "body", source = j) %>%
+  autofit( add_w = 0.1, add_h = 0.1, part = c("body", "header")) %>%
   merge_v(~Treatment) %>%
   fix_border_issues()
 
